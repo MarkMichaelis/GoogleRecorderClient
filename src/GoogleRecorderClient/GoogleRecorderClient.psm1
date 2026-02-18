32abd013c9fc120b4bd35bc7a7cbc22266de770a
Mark Michaelis <mark@michaelis.net>
2026-02-18 00:19:02 -0800

    test(transcript): fix PSTypeNames assertion and pipeline mock data - 5/5 green

--- src/GoogleRecorderClient/GoogleRecorderClient.psm1
+++ src/GoogleRecorderClient/GoogleRecorderClient.psm1
@@ -51,5 +51,6 @@ Export-ModuleMember -Function @(
     'Connect-GoogleRecorder'
     'Disconnect-GoogleRecorder'
     'Get-GoogleRecording'
+    'Get-GoogleRecordingTranscript'
     'Save-GoogleRecordingAudio'
 )
